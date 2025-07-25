89a363c2905dac70a1785d45ae3a577dd95766f9
Sofia Vaz <sofiavazdacostaxavier@gmail.com>
2025-07-25 18:49:08 -0300

    feat: adicionando filtro de query na rota cursos para filtrar por data

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -1,5 +1,7 @@
+const { Op } = require('sequelize'); 
 const Controller = require('./Controller.js');
 const CursoServices = require('../services/CursoServices.js');
+
 
 const cursoServices = new CursoServices();
 
@@ -7,6 +9,30 @@ class CursoController extends Controller {
   constructor() {
     super(cursoServices);
   }
+
+  async pegaCursos (req, res) {
+    const {data_inicial, data_final} = req.query; 
+    const where = {};
+  
+    //se existirem os params, criar uma prop {}
+    data_inicial || data_final ? where.data_inicio = { } : null ;
+    //se existir data inicial, adiciona a propriedade greater than, prop gte 
+    data_inicial ? where.data_inicio[Op.gte] = data_inicial : null; 
+    //se existir data final, idem 
+    data_final ? where.data_inicio[Op.lte] = data_final : null;
+
+    try {
+      const listaCursos = await cursoServices.pegaTodosOsRegistros(where); 
+       return res.status(200).json(listaCursos);
+    } catch {
+      return res.status(500).json({erro: erro.message})
+    }
+
+
+    
+  }
+
+
 }
 
 module.exports = CursoController;
